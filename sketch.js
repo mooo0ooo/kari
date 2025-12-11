23068de08ad2bc84e3a326963e6a17cecc92099b
mooo0ooo <mymichael5000@gmail.com>
2025-12-11 23:17:28 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -867,12 +867,9 @@ function drawPADButtons(){
 	
 　const colors = colorPatterns[currentColorPattern];
 
-  fill(255);
-  textAlign(CENTER, CENTER);
-  textSize(btnSize * 0.4);
+  drawPADLavels();
 
   // P 行
-  text("P", centerX, centerY - 120 * padLayout.scl);
   for(let i = 0; i < 7; i++) {
     let amount = i / 6;
     let r = lerp(colors.P.start[0], colors.P.end[0], amount);
@@ -890,7 +887,6 @@ function drawPADButtons(){
   }
 	
   // A 行
-  text("A", centerX, centerY);
   for(let i = 0; i < 7; i++) {
     let amount = i / 6;
     let r = lerp(colors.A.start[0], colors.A.end[0], amount);
@@ -910,7 +906,6 @@ function drawPADButtons(){
   }
 	
   // D 行
-  text("D", centerX, centerY + 120 * padLayout.scl);
   for(let i = 0; i < 7; i++) {
     let col = color(colors.D[0], colors.D[1], colors.D[2]);
     let sides = i + 3;
@@ -923,6 +918,35 @@ function drawPADButtons(){
                "polygon", 
                sides);
   }
+  
+  pop();
+}
+
+function drawPADLabels() {
+  push();
+  textAlign(CENTER, CENTER);
+  textSize(btnSize * 0.4);
+  fill(255);
+  
+  const centerX = width / 2;
+  const centerY = height / 2;
+  const buttonSize = btnSize * 0.8;
+  
+  // P
+  const pX = centerX;
+  const pY = centerY - 120 * padLayout.scl;
+  
+  // A
+  const aX = centerX;
+  const aY = centerY;
+  
+  // D
+  const dX = centerX;
+  const dY = centerY + 120 * padLayout.scl;
+
+  text("P", pX, pY);
+  text("A", aX, aY);
+  text("D", dX, dY);
   
   pop();
 }
