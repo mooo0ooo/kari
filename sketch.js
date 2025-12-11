85d6a4d3e4cbc1d254dc3c4b596b1d18795260ec
mooo0ooo <mymichael5000@gmail.com>
2025-12-11 21:16:13 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -824,12 +824,13 @@ function drawPADButtons(){
 
   // 案内文を追加
   textSize(16);
-  textAlign(CENTER, CENTER);
+  textAlign(CENTER, TOP);
   fill(255);
   text("今の気分に合う色や形をP,A,D１つずつ選んでください", cx, cy - 200);
-  
+
+  // PADのスタイル
   textSize(24);
-  textAlign(CENTER, TOP);
+  textAlign(RIGHT, CENTER);
 
   // P 行
   text("P", cx - 150, cy - 120);
